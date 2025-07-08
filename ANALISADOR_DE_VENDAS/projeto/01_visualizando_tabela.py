837283a5399d8bb01477756b50ae8d08a6b1a475
guidonatooo <guidonatoo@gmail.com>
2025-07-08 12:12:59 -0300

    last changes ANALISADOR_DE_VENDAS

---
+++ ANALISADOR_DE_VENDAS/projeto/01_visualizando_tabela.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import streamlit as st
+import pandas as pd
+
+
+pasta_datasets = Path(__file__).parent.parent / 'datasets'
+caminho_vendas = pasta_datasets / 'vendas.csv'
+
+df_vendas = pd.read_csv(caminho_vendas, sep=';', decimal=',')
+
+st.dataframe(df_vendas)
+
+
+
+
